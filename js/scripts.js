11ed59f0e0e76800bd365fde205f2c377acd3d58
epicodus <student@epicodus.com>
2014-02-24 17:26:21 -0800

    jquery not complete

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,62 @@
 // testShipping.shipDate
 // testShipping.pickup
 
-    var Package = {
-      pickupFee: function() {
-       if (this.pickup === true) {
-        return 5;
-      } else 
+var Package = {
+  pickupFee: function() {
+    if (this.pickup === "pickup") {
+      return 5;
+    } else {
       return 0; 
+    }
+  },
+  weightFee: function() {
+    return this.weight;
+  },
+  withinCountryFee: function() {
+    if (this.toCountry === this.fromCountry) {
+      return 10;
+    } else {
+      return 25;
+    }
+  },
+  withinZipFee: function() {
+    if (this.toZip === this.fromZip) {
+      return 0;
+    }
   }
-      weightFee: function() {
-        return this.weight;
-      }
 };
+
+
+$(document).ready(function(){
+  $('form#shipment-details').submit(function(event) {
+    event.preventDefault();
+
+    var inputFromCountry = $("input#from-country").val();
+    var inputFromZip = $("input#from-zip").val();
+    var inputToCountry = $("input#to-country").val();
+    var inputToZip = $("input#to-zip").val();
+    var inputWeight = $("input#weight").val();
+    var inputPickup = $("input#pickup").val();
+    var newShipment = Object.create(Package);
+    newShipment.fromCountry = inputFromCountry;
+    newShipment.fromZip = inputFromZip;
+    newShipment.toCountry = inputToCountry;
+    newShipment.toZip = inputToZip;
+    newShipment.weight = inputWeight;
+    newShipment.pickup = inputPickup;
+
+    var totalCost = parseInt(newShipment.pickupFee()) + parseInt(newShipment.weightFee()) + parseInt(newShipment.withinCountryFee()) + parseInt(newShipment.withinZipFee());
+console.log(newShipment.pickup)
+console.log(newShipment.pickupFee());
+
+    $("h2#results").append("Your shipment will cost: $" + totalCost);
+
+
+    this.reset();
+  });
+});
+
+
+
 
 
--- specs/specs.js
+++ specs/specs.js
@@ -7,11 +7,35 @@ describe('Package', function() {
     });
   });
 
-    describe('weightFee', function() {
-      it('should equal one dollar per every pound', function() {
+  describe('weightFee', function() {
+    it('should equal one dollar per every pound', function() {
       var testShipping = Object.create(Package);
       testShipping.weight = 7;
       testShipping.weightFee().should.equal(7);
+    });
+  });
+
+  describe('withinCountryFee', function() {
+    it('should return 10 if to country and from country are the same', function() {
+      var testShipping = Object.create(Package);
+      testShipping.toCountry = "USA";
+      testShipping.fromCountry = "USA";
+      testShipping.withinCountryFee().should.equal(10);
+    });
+
+    it('should return 25 if to country and from country are not same', function() {
+      var testShipping = Object.create(Package);
+      testShipping.toCountry = "USA";
+      testShipping.fromCountry = "Spain";
+      testShipping.withinCountryFee().should.equal(25);
+    });
+  });
+  describe('withinZipFee', function() {
+    it('should equal 0 when inside country item was shipped to', function() {
+      var testShipping = Object.create(Package);
+      testShipping.toZip = "97214";
+      testShipping.fromZip = "97214";
+      testShipping.withinZipFee().should.equal(0);
     });
   });
 });
